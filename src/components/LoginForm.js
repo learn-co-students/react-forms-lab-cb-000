ae065dc8583cf111785c78ce2c285943ee275aec
theboymo <billfero@gmail.com>
2018-05-23 19:33:53 +0100

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,58 @@
 import React from "react";
 
-class LoginForm extends React.Component {
+export default class LoginForm extends React.Component {
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+  usernameChangeHandler = (e) => {
+    this.setState({
+      username: e.target.value
+    });
+  };
+
+  passwordChangeHandler = (e) => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const username = this.state.username;
+    const password = this.state.password;
+    if(username && password){
+      this.props.onSubmit({ username, password });
+    }
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={ this.formSubmit }>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input 
+              id="test-username" 
+              type="text" 
+              name="username" 
+              value={ this.state.username } 
+              onChange={ this.usernameChangeHandler } />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input 
+              id="test-password" 
+              type="password" 
+              value={ this.state.password } 
+              onChange={ this.passwordChangeHandler }
+            />
           </label>
         </div>
         <div>
@@ -30,4 +63,3 @@ class LoginForm extends React.Component {
   }
 }
 
-export default LoginForm;
