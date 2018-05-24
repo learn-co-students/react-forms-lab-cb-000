09ce1feb49589a331b507559b626149309d5bde9
Heather Christman <efl7a@yahoo.de>
2018-05-24 15:03:52 -0400

    error tests fail even though it works in the browser

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,49 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleInputChange = (event) => {
+    const value = event.target.value
+    const name = event.target.name
+
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const {username, password } = this.state
+    if(username.length > 0 && password.length > 0) {
+      this.props.onSubmit(username, password)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" name="username" type="text" value={this.state.username} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" name="password" type="password" value={this.state.password} onChange={this.handleInputChange}/>
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" >Log in</button>
         </div>
       </form>
     );
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,49 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: "",
+      notValid: true
+    };
+  }
+
+  validatePoem = () => {
+      const poemArray = this.state.value.split(/[\r\n]/)
+      const linecount = poemArray.length
+      if(linecount === 3){
+        const firstLineCount = poemArray[0].replace(/\s+$/g,"").split(" ").length
+        const secondLineCount = poemArray[1].replace(/\s+$/g,"").split(" ").length
+        const thirdLineCount = poemArray[2].replace(/\s+$/g,"").split(" ").length
+        if(firstLineCount === 5 && secondLineCount === 3 && thirdLineCount === 5) {
+          this.setState({
+            notValid: false
+          })
+        } else {
+          this.setState({
+            notValid: true
+          })
+        }
+      } else {
+        this.setState({
+          notValid: true
+        })
+      }
+    }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value
+    }, this.validatePoem())
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleChange}/>
+          {this.state.notValid ?
+            (<div id="poem-validation-error" style={{ color: "red" }} >
+            This poem is not written in the right structure!
+          </div>):null}
       </div>
     );
   }
