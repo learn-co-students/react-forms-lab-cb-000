6ebcaf2a153c3ac8b77c72f2e1d05b47574288b0
Felice Forby <forby.1@gmail.com>
2019-03-17 14:42:50 +0900

    add LoginForm and TwitterMessage compontents

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,53 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+  
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if (this.state.username || this.state.password) {      
+      this.props.onSubmit(this.state)
+    } else {
+      console.log("Provide both username and password")
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              onChange={this.handleChange}
+              value={this.state.username}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
           </label>
         </div>
         <div>
