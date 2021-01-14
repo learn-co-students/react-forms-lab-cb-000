1de43e9e7d0e3e924825831558871d09e0122b3b
s-blais <stephen@s-blais.com>
2021-01-14 17:14:24 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -27,22 +27,19 @@ class LoginForm extends React.Component {
           <label>
             Username
             <input
-              id="username"
-              name="username"
-              type="text"
+              id="username" name="username" type="text" 
               onChange={this.handleChange} 
-              value={this.state.username} />
+              value={this.state.username} 
+            />
           </label>
         </div>
         <div>
           <label>
             Password
             <input 
-              id="password" 
-              name="password" 
-              type="password" 
+              id="password" name="password" type="password" 
               onChange={this.handleChange} 
-              value={this.state.password}
+              value={this.state.password} 
             />
           </label>
         </div>
