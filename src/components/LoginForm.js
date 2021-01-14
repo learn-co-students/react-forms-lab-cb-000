7b2e70a4cbae78dd7ebd96decbaf35481c77c9fb
s-blais <stephen@s-blais.com>
2021-01-14 17:12:09 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,49 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+  state = {
+    // it works even if these aren't set BUT it throws a warning in the console that you're switching from uncontrolled to controlled
+    username: "",
+    password: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.state.username != "" && this.state.password != "") {
+    this.props.handleLogin(this.state)}
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              onChange={this.handleChange} 
+              value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+              id="password" 
+              name="password" 
+              type="password" 
+              onChange={this.handleChange} 
+              value={this.state.password}
+            />
           </label>
         </div>
         <div>
